f81435380a3a70405ea4ee665ff2a4c30471c6ce
Sardor <sindarovasardor9@gmail.com>
2022-11-23 01:55:38 +0500

    12

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function ret() {
+  let val = document.querySelector("input").value;
+  document.getElementById("num").innerHTML = val;
+  let time = new Date(val).getFullYear();
+  let timr = new Date(val).getMonth() + 1;
+  let tiwe = new Date(val).getDate();
+  let tran = `${timr}-${tiwe}-${time}`;
+  let perm = new Date();
+  let now = new Date(tran);
+  let month = Math.round((perm - now) / 1000 / 60 / 60 / 24);
+  document.getElementById("yil").innerHTML = Math.round(
+    (perm - now) / 1000 / 60 / 60 / 24 / 365.25
+  );
+  document.getElementById("oy").innerHTML = Math.round(month / 30.4375);
+  document.getElementById("kun").innerHTML = Math.round(
+    (perm - now) / 1000 / 60 / 60 / 24
+  );
+  document.getElementById("soat").innerHTML = Math.round(
+    (perm - now) / 1000 / 60 / 60
+  );
+  document.getElementById("minut").innerHTML = Math.round(
+    (perm - now) / 1000 / 60
+  );
+  document.getElementById("sekund").innerHTML = Math.round((perm - now) / 1000);
+  document.getElementById("milsek").innerHTML = Math.round(perm - now);
+}
