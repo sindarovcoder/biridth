cefc4b53151f319f6c81c4349d5b8f8ff58accce
Sindarov Sardor <97356814+Sardorcodee@users.noreply.github.com>
2022-11-23 23:41:11 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,20 +7,20 @@ function ret() {
   let tran = `${timr}-${tiwe}-${time}`;
   let perm = new Date();
   let now = new Date(tran);
-  let month = Math.round((perm - now) / 1000 / 60 / 60 / 24);
-  document.getElementById("yil").innerHTML = Math.round(
+  let month = Math.floor((perm - now) / 1000 / 60 / 60 / 24);
+  document.getElementById("yil").innerHTML = Math.floor(
     (perm - now) / 1000 / 60 / 60 / 24 / 365.25
   );
-  document.getElementById("oy").innerHTML = Math.round(month / 30.4375);
-  document.getElementById("kun").innerHTML = Math.round(
+  document.getElementById("oy").innerHTML = Math.floor(month / 30.4375);
+  document.getElementById("kun").innerHTML = Math.floor(
     (perm - now) / 1000 / 60 / 60 / 24
   );
-  document.getElementById("soat").innerHTML = Math.round(
+  document.getElementById("soat").innerHTML = Math.floor(
     (perm - now) / 1000 / 60 / 60
   );
-  document.getElementById("minut").innerHTML = Math.round(
+  document.getElementById("minut").innerHTML = Math.floor(
     (perm - now) / 1000 / 60
   );
-  document.getElementById("sekund").innerHTML = Math.round((perm - now) / 1000);
-  document.getElementById("milsek").innerHTML = Math.round(perm - now);
+  document.getElementById("sekund").innerHTML = Math.floor((perm - now) / 1000);
+  document.getElementById("milsek").innerHTML = Math.floor(perm - now);
 }
